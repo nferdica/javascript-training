dba98843d011f069bab1532045128d695a31029e
Nathan Ferdinando <nferdica@outlook.com>
2024-07-29 19:54:17 -0300

    Creating boilerplate

--- node/views/server.js
+++ node/views/server.js
@@ -2,10 +2,15 @@ const express = require("express"); // Importando express
 const app = express(); // Carregando express
 const routes = require('./routes')
 const path = require('path')
+const {middlewareGlobal} = require('./src/middlewares/middlewares')
 
 app.use(express.urlencoded({extended: true}))
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs')
+app.use(express.static(path.resolve(__dirname, 'public'))) // Setando caminho dos arquivos estáticos.
+app.set('views', path.resolve(__dirname, 'src', 'views')) // Setando caminho da pasta views.
+app.set('view engine', 'ejs') // Setando engine...
+
+// Middlewares próprios.
+app.use(middlewareGlobal)
 app.use(routes)
 
 app.listen(3000, () => {
