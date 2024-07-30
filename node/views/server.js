d70a307c2bf9a94ef29141bd981d6d24c8aecf20
Nathan Ferdinando <nferdica@outlook.com>
2024-07-29 21:41:41 -0300

    Introduction to session and flash messages

--- node/views/server.js
+++ node/views/server.js
@@ -11,12 +11,30 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     })
     .catch(e => console.log(e))
 
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash')
+
 const routes = require('./routes')
 const path = require('path')
 const {middlewareGlobal} = require('./src/middlewares/middlewares')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.resolve(__dirname, 'public'))) // Setando caminho dos arquivos estáticos.
+
+const sessionOptions = session({
+    secret: 'qwer qwer qwer',
+    store: MongoStore.create({mongoUrl: process.env.CONNECTIONSTRING}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+    }
+})
+app.use(sessionOptions);
+app.use(flash())
+
 app.set('views', path.resolve(__dirname, 'src', 'views')) // Setando caminho da pasta views.
 app.set('view engine', 'ejs') // Setando engine...
 
