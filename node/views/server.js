21b469d912a6af15091cdbb8970e4d951f9591ca
Nathan Ferdinando <nferdica@outlook.com>
2024-07-29 21:12:16 -0300

    Connecting database and creating modules

--- node/views/server.js
+++ node/views/server.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const express = require("express"); // Importando express
 const app = express(); // Carregando express
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        app.emit('online')
+        console.log('BANCO DE DADOS: Online')
+    })
+    .catch(e => console.log(e))
+
 const routes = require('./routes')
 const path = require('path')
 const {middlewareGlobal} = require('./src/middlewares/middlewares')
@@ -13,7 +24,9 @@ app.set('view engine', 'ejs') // Setando engine...
 app.use(middlewareGlobal)
 app.use(routes)
 
-app.listen(3000, () => {
-    console.log('ACESSAR: http://localhost:3000');
-    console.log('PORT: 3000')
-});
+app.on('online', () => {
+    app.listen(3000, () => {
+        console.log('ACESSAR: http://localhost:3000');
+        console.log('PORT: 3000')
+    });
+})
